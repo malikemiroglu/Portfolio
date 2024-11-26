5c0d3f738ab2c550129e4e3090cb716d15c99122
Malik <malikemiroglu92@gmail.com>
2024-11-26 13:24:05 +0300

    update

--- src/Cards/ProjectsCardsStyle.js
+++ src/Cards/ProjectsCardsStyle.js
@@ -41,6 +41,7 @@ export const Image = styled.img`
     background-color: ${({ theme }) => theme.white};
     border-radius: 10px;
     box-shadow: 0 0 16px 2px rgba(0,0,0,0.3);
+    object-fit: cover;
 `;
 
 export const Tags = styled.div`
